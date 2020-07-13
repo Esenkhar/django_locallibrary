c5f2050bff916fe7380d7464dad356a19077bda2
Esen <esenkharkov@gmail.com>
2020-07-13 15:15:26 +0300

    Remove db.sqlite3 from git

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -74,14 +74,6 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'locallibrary',
-    #     'USER': 'esen',
-    #     'PASSWORD': 'tip-top1',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
-    # },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
