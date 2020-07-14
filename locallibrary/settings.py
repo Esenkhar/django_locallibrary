9a6af1e41a5020d20723212b78828a5d0bd25d17
Esen <esenkharkov@gmail.com>
2020-07-14 12:34:36 +0300

    Add DATABASES from settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':
+        {'ENGINE': 'django.db.backends.postgresql', }
 }
 
 # Heroku: Update database configuration from $DATABASE_URL.
