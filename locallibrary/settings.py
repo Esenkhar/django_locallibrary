193954bf81e8e6d399c2a390f4e1d00ea24624d8
Esen <esenkharkov@gmail.com>
2020-07-14 11:51:00 +0300

    Remove DATABASES from settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
